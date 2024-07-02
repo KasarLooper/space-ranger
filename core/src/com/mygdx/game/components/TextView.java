fd77d0cf5a80b403beb2c39af2aaf1d43b7172eb
juliaVenichenko <LeoVeDm@yandex.ru>
2024-07-02 13:40:47 +0300

    Created class: ButtonView, FontBuilder, MenuScreen,MovingBackground(unfinished) and TextView Add methods and variables in class: GameResources, MyGdxGame, SpaceGameScreen(add background), View Delete picture "badlogic", add picture "ship"

---
+++ core/src/com/mygdx/game/components/TextView.java
@@ -0,0 +1,40 @@
+package com.mygdx.game.components;
+
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class TextView extends View{
+    protected BitmapFont font;
+    protected String text;
+
+    public TextView(BitmapFont font, float x, float y) {
+        super(x, y);
+        this.font = font;
+    }
+
+    public TextView(BitmapFont font, float x, float y, String text) {
+        this(font, x, y);
+        this.text = text;
+
+        GlyphLayout glyphLayout = new GlyphLayout(font, text);
+        width = glyphLayout.width;
+        height = glyphLayout.height;
+    }
+    public void setText(String text) {
+        this.text = text;
+        GlyphLayout glyphLayout = new GlyphLayout(font, text);
+        width = glyphLayout.width;
+        height = glyphLayout.height;
+    }
+    @Override
+    public void draw(SpriteBatch batch) {
+        font.draw(batch, text, x, y + height);
+    }
+
+    @Override
+    public void dispose() {
+        font.dispose();
+    }
+}
+
