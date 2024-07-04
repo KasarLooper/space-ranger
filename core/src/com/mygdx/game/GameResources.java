56b25b5c0d8b70300334348356bac719ee6d6b79
juliaVenichenko <LeoVeDm@yandex.ru>
2024-07-04 14:00:09 +0300

    Add variables for anim_ship_portal, add picture for anim_ship_portal.

--- core/src/com/mygdx/game/GameResources.java
+++ core/src/com/mygdx/game/GameResources.java
@@ -10,4 +10,11 @@ public class GameResources {
     public static final String BUTTON_IMG_PATH = "button_bg.png";
     public static final String CORE_IMG_PATH = "core.png";
     public static final String ENEMY_SHIP_IMG_PATH = "Enemy_ship.png";
+
+    //Анимация перехода корабля в кротовую нору
+    public static final String ANIM_SHIP_PORTAL_1 = "anim_ship_portal_1.png";
+    public static final String ANIM_SHIP_PORTAL_2 = "anim_ship_portal_2.png";
+    public static final String ANIM_SHIP_PORTAL_3 = "anim_ship_portal_3.png";
+    public static final String ANIM_SHIP_PORTAL_4 = "anim_ship_portal_4.png";
+    public static final String ANIM_SHIP_PORTAL_5 = "anim_ship_portal_5.png";
 }
