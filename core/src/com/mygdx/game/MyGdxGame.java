7ea40cfc1547eea7c7fbb01e56cb68af5a3a1550
KasarLooper <kasar.looper@gmail.com>
2024-07-06 20:58:15 +0800

    Temp

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -42,7 +42,6 @@ public class MyGdxGame extends Game {
 	public AudioManager audioManager;
 	@Override
 	public void create () {
-		MemoryManager.saveIsNextLevel(false);
 		canAccessPlanetLevel = MemoryManager.loadIsNextLevel();
 		Box2D.init();
 		world = new World(new Vector2(0, 0), true);
