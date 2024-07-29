d6396f3170e27e7bccdcf7be1ad6e6b3d361319c
KasarLooper <kasar.looper@gmail.com>
2024-07-29 18:14:37 +0800

    Compress to jpg

--- core/src/com/mygdx/game/GameResources.java
+++ core/src/com/mygdx/game/GameResources.java
@@ -6,8 +6,8 @@ public class GameResources {
     public static final String JOYSTICK_BACK_IMG_PATH = "joystick-zone.png";
     public static final String JOYSTICK_CIRCLE_IMG_PATH = "joystick-circle.png";
     public static final String BULLET_IMG_PATH = "bullet.png";
-    public static final String BACKGROUND_IMG_PATH = "background.png";
-    public static final String BACKGROUND_MENU_IMG_PATH = "background_menu.png";
+    public static final String BACKGROUND_IMG_PATH = "background.jpg";
+    public static final String BACKGROUND_MENU_IMG_PATH = "background_menu.jpg";
 
     public static final String BACKGROUND_2_IMG_PATH = "back_2.png";
     public static final String BLACKOUT_IMG_PATH = "blackout_full.png";
@@ -24,26 +24,6 @@ public class GameResources {
     public static final String BOOM_IMG_PATH = "Boom.png";
     public static final String ENEMY_BULLET_IMG_PATH = "enemy_bullet.png";
 
-    //Анимация перехода корабля в кротовую нору
-    public static final String ANIM_SHIP_PORTAL_1 = "anim_ship_portal_1.png";
-    public static final String ANIM_SHIP_PORTAL_2 = "anim_ship_portal_2.png";
-    public static final String ANIM_SHIP_PORTAL_3 = "anim_ship_portal_3.png";
-    public static final String ANIM_SHIP_PORTAL_4 = "anim_ship_portal_4.png";
-    public static final String ANIM_SHIP_PORTAL_5 = "anim_ship_portal_5.png";
-    public static final String ANIM_SHIP_PORTAL_6 = "anim_ship_portal_6.png";
-    public static final String ANIM_SHIP_PORTAL_7 = "anim_ship_portal_7.png";
-    public static final String ANIM_SHIP_PORTAL_8 = "anim_ship_portal_8.png";
-    public static final String ANIM_SHIP_PORTAL_9 = "anim_ship_portal_9.png";
-    public static final String ANIM_SHIP_PORTAL_10 = "anim_ship_portal_10.png";
-    public static final String ANIM_SHIP_PORTAL_11 = "anim_ship_portal_11.png";
-    public static final String ANIM_SHIP_PORTAL_12 = "anim_ship_portal_12.png";
-    public static final String ANIM_SHIP_PORTAL_13 = "anim_ship_portal_13.png";
-    public static final String ANIM_SHIP_PORTAL_14 = "anim_ship_portal_14.png";
-    public static final String ANIM_SHIP_PORTAL_15 = "anim_ship_portal_15.png";
-    public static final String ANIM_SHIP_PORTAL_16 = "anim_ship_portal_16.png";
-    public static final String ANIM_SHIP_PORTAL_17 = "anim_ship_portal_17.png";
-    public static final String ANIM_SHIP_PORTAL_18 = "anim_ship_portal_18.png";
-    public static final String ANIM_SHIP_PORTAL_19 = "anim_ship_portal_19.png";
     public static final String ANIM_SHIP_PORTAL_IMG_PATH_PATTERN = "anim_ship_portal/anim_ship_portal_%d.png";
     public static final String FIRE_BUTTON_IMG_PATH = "Remove-bg.ai_1720009081104.png";
     public static final String PAUSE_ICON_IMG_PATH = "pause_icon.png";
@@ -75,56 +55,16 @@ public class GameResources {
     public static final String TEXTURE_BOX_GREEN = "texture_box_1.png";
     public static final String TEXTURE_BOX_BLACK = "texture_box_2.png";
 
-    //cosmonaut_anim_right
-    public static final String COSMONAUT_ANIM_RIGHT_1 = "cosmonaut_anim_right_1.png";
-    public static final String COSMONAUT_ANIM_RIGHT_2 = "cosmonaut_anim_right_2.png";
-    public static final String COSMONAUT_ANIM_RIGHT_3 = "cosmonaut_anim_right_3.png";
-    public static final String COSMONAUT_ANIM_RIGHT_4 = "cosmonaut_anim_right_4.png";
-    public static final String COSMONAUT_ANIM_RIGHT_5 = "cosmonaut_anim_right_5.png";
-    public static final String COSMONAUT_ANIM_RIGHT_6 = "cosmonaut_anim_right_6.png";
-    public static final String COSMONAUT_ANIM_RIGHT_7 = "cosmonaut_anim_right_7.png";
     public static final String COSMONAUT_ANIM_RIGHT_IMG_PATTERN = "cosmonaut_anim_right/cosmonaut_anim_right_%d.png";
-
-    //cosmonaut_anim_left
-
-    public static final String COSMONAUT_ANIM_LEFT_1 = "cosmonaut_anim_left/cosmonaut_anim_left_1.png";
-    public static final String COSMONAUT_ANIM_LEFT_2 = "cosmonaut_anim_left_2.png";
-    public static final String COSMONAUT_ANIM_LEFT_3 = "cosmonaut_anim_left_3.png";
-    public static final String COSMONAUT_ANIM_LEFT_4 = "cosmonaut_anim_left_4.png";
-    public static final String COSMONAUT_ANIM_LEFT_5 = "cosmonaut_anim_left_5.png";
-    public static final String COSMONAUT_ANIM_LEFT_6 = "cosmonaut_anim_left_6.png";
-    public static final String COSMONAUT_ANIM_LEFT_7 = "cosmonaut_anim_left_7.png";
     public static final String COSMONAUT_ANIM_LEFT_IMG_PATTERN = "cosmonaut_anim_left/cosmonaut_anim_left_%d.png";
 
     public static final String JUMP_BUTTON_IMG_PATH = "upbutton.png";
 
     //Alien
-    //Alien_anim_right
-    public static final String ALIEN_ANIM_RIGHT_1 = "Alien_anim_right/alien_anim_right_1.png";
-    public static final String ALIEN_ANIM_RIGHT_2 = "Alien_anim_right/alien_anim_right_2.png";
-    public static final String ALIEN_ANIM_RIGHT_3 = "Alien_anim_right/alien_anim_right_3.png";
-    public static final String ALIEN_ANIM_RIGHT_4 = "Alien_anim_right/alien_anim_right_4.png";
-    public static final String ALIEN_ANIM_RIGHT_5 = "Alien_anim_right/alien_anim_right_5.png";
-    public static final String ALIEN_ANIM_RIGHT_6 = "Alien_anim_right/alien_anim_right_6.png";
-    public static final String ALIEN_ANIM_RIGHT_7 = "Alien_anim_right/alien_anim_right_7.png";
-    public static final String ALIEN_ANIM_RIGHT_8 = "Alien_anim_right/alien_anim_right_8.png";
-    public static final String ALIEN_ANIM_RIGHT_9 = "Alien_anim_right/alien_anim_right_9.png";
-
-    //Alien_anim_left
-    public static final String ALIEN_ANIM_LEFT_1 = "Alien_anim_left/alien_anim_left_1.png";
-    public static final String ALIEN_ANIM_LEFT_2 = "Alien_anim_left/alien_anim_left_2.png";
-    public static final String ALIEN_ANIM_LEFT_3 = "Alien_anim_left/alien_anim_left_3.png";
-    public static final String ALIEN_ANIM_LEFT_4 = "Alien_anim_left/alien_anim_left_4.png";
-    public static final String ALIEN_ANIM_LEFT_5 = "Alien_anim_left/alien_anim_left_5.png";
-    public static final String ALIEN_ANIM_LEFT_6 = "Alien_anim_left/alien_anim_left_6.png";
-    public static final String ALIEN_ANIM_LEFT_7 = "Alien_anim_left/alien_anim_left_7.png";
-    public static final String ALIEN_ANIM_LEFT_8 = "Alien_anim_left/alien_anim_left_8.png";
-    public static final String ALIEN_ANIM_LEFT_9 = "Alien_anim_left/alien_anim_left_9.png";
-
     public static final String ALIEN_ANIM_RIGHT_IMG_PATTERN = "Alien_anim_right/alien_anim_right_%d.png";
     public static final String ALIEN_ANIM_LEFT_IMG_PATTERN = "Alien_anim_left/alien_anim_left_%d.png";
 
     //Заставка для конца игры
-    public static final String BACKGROUND_END_IMG_PATH = "background_end.png";
+    public static final String BACKGROUND_END_IMG_PATH = "background_end.jpg";
     public static final String FIRE_BUTTON_PLANET_IMG_PATH = "fireButtonPlanet.png";
 }
