a0c402b9d63596341066706e416b6cffe66bf607
KasarLooper <kasar.looper@gmail.com>
2024-08-16 22:51:56 +0800

    Fix alien behavior

--- core/src/com/kasarlooper/spaceranger/objects/AlienObject.java
+++ core/src/com/kasarlooper/spaceranger/objects/AlienObject.java
@@ -18,14 +18,12 @@ import java.util.Collections;
 public class AlienObject extends SpacemanObject{
     long lastCheckTime1, lastCheckTime2;
     float lastX, lastY;
-    boolean isChangingDirection;
     private long stopTime;
 
     public AlienObject(int x, int y, int wight, int height, String texturePath, int defaultFrame, int speed, int jumpImpulse, World world, BlockMap blockMap) {
         super(x, y, wight, height, texturePath, defaultFrame, speed, jumpImpulse, world, blockMap);
         lastCheckTime1 = TimeUtils.millis();
         lastX = getX();
-        isChangingDirection = false;
         stopTime = 0;
     }
 
@@ -68,81 +66,9 @@ public class AlienObject extends SpacemanObject{
     public void move(int playerX, int playerY, ArrayList<PhysicsBlock> blocks) {
         if (TimeUtils.millis() - stopTime < 300) return;
         boolean isRight = playerX > getX();
-        if (!isChangingDirection) jump();
-        if (isStuck()) {
-            isChangingDirection = true;
-            lastCheckTime2 = TimeUtils.millis();
-            lastY = getY();
-        }
-        if (isChangingDirection) isRight = !isRight;
+        jump();
         if (isRight) stepRight();
         else stepLeft();
-    }
-
-    private boolean isStuck() {
-        if (TimeUtils.millis() - lastCheckTime1 > 2000) {
-            lastCheckTime1 = TimeUtils.millis();
-            if (lastX == getX()) return true;
-            else {
-                lastX = getX();
-                return false;
-            }
-        } else return false;
-    }
-
-    private boolean isChangedDirection() {
-        if (TimeUtils.millis() - lastCheckTime2 > 3000) {
-            lastCheckTime2 = TimeUtils.millis();
-            if (lastY != getY()) {
-                lastY = getY();
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private boolean isInAir(ArrayList<PhysicsBlock> blocks) {
-        for (PhysicsBlock block : blocks) {
-            if (getY() - block.getY() == (COSMONAUT_HEIGHT + BLOCK_SIZE) / 2f &&
-                    Math.abs(getX() - block.getX()) <= BLOCK_SIZE / 2f) return false;
-        }
-        return true;
-    }
-
-    private boolean isReachPlayer(int playerX) {
-        return Math.abs(getX() - playerX) <= BLOCK_SIZE / 2f;
-    }
-
-    private boolean hasToJump(ArrayList<PhysicsBlock> blocks, boolean isRight) {
-        ArrayList<Pair> distances = getBlockDistances(blocks, isRight);
-        int height = getCeilHeight(distances);
-        if (height == 0) return false;
-        for (Pair cords : distances) {
-            if (cords.y <= 0) continue;
-            if (cords.x == 1 && cords.y <= height) {
-                System.out.printf("%d %d\n", cords.x, cords.y);
-                return true;
-            }
-            if (height == 1) continue;
-            if (cords.x == 2 && cords.y <= (height - 2) * 3) {
-                System.out.printf("%d %d\n", cords.x, cords.y);
-                return true;
-            }
-            if (height == 2) continue;
-            if (cords.x == 3 && cords.y <= 1) {
-                System.out.printf("%d %d\n", cords.x, cords.y);
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private int getCeilHeight(ArrayList<Pair> distances) {
-        for (Pair pair : distances) {
-            if (pair.x > 1) return 3;
-            if (pair.y < 5) return pair.y - 2;
-        }
-        return 3;
     }
 
     private ArrayList<Pair> getBlockDistances(ArrayList<PhysicsBlock> blocks, boolean isRight) {
